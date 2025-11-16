6fc07e92b5aa94e05372f58756fde76a60d95906
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-11-16 20:03:21 +0300

    add docs page

--- .source/index.ts
+++ .source/index.ts
@@ -1,4 +1,5 @@
 // @ts-nocheck -- skip type checking
+import * as d_docs_0 from "../content/docs/(root)/index.mdx?collection=docs"
 import { _runtime } from "fumadocs-mdx/runtime/next"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([], [{"info":{"path":"meta.json","fullPath":"content\\docs\\meta.json"},"data":{"pages":[],"root":true}}])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"(root)/index.mdx","fullPath":"content\\docs\\(root)\\index.mdx"}, data: d_docs_0 }], [{"info":{"path":"meta.json","fullPath":"content\\docs\\meta.json"},"data":{"pages":[],"root":true}}, {"info":{"path":"(root)/meta.json","fullPath":"content\\docs\\(root)\\meta.json"},"data":{"title":"Get Started","pages":["index"]}}])
