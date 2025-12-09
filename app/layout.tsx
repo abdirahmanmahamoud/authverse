52c291b02f311ffe5ac165694e5070ae143a830c
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-12-09 10:24:11 +0300

    refactor(docs/github ac): Add GitHub Actions build workflow and Next.js root layout with metadata and theme provider, and update core dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import "@/app/global.css";
 import ThemeProvider from "@/components/themeProvider";
-import { RootProvider } from "fumadocs-ui/provider";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import type { ReactNode } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -56,20 +54,18 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </RootProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
