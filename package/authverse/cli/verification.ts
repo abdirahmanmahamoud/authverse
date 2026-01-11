fa1b9b8e2c400b03dc35ca10895c841ab048ca55
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2026-01-11 15:57:53 +0300

    feat: add email verification functionality and CLI command

---
+++ package/authverse/cli/verification.ts
@@ -0,0 +1,30 @@
+import chalk from "chalk";
+import fs from "fs";
+import path from "path";
+import { verifyNext } from "../script/verifyNext.js";
+
+export const verification = async () => {
+  try {
+    const projectDir = process.cwd();
+    const packageJsonPath = path.join(projectDir, "package.json");
+
+    // Auto detect framework
+    let framework: "Next js" | "tanstack state" = "tanstack state";
+
+    if (fs.existsSync(packageJsonPath)) {
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+
+      const hasNext =
+        packageJson?.dependencies?.["next"] ||
+        packageJson?.devDependencies?.["next"];
+
+      framework = hasNext ? "Next js" : "tanstack state";
+    }
+
+    if (framework === "Next js") {
+      await verifyNext();
+    }
+  } catch (error: any) {
+    console.log(chalk.red(error.message));
+  }
+};
--- package/authverse/index.ts
+++ package/authverse/index.ts
@@ -7,6 +7,7 @@ import { forget } from "./cli/forget.js";
 import { isNextJsProject } from "./script/detect-nextjs.js";
 import chalk from "chalk";
 import { email } from "./cli/email.js";
+import { verification } from "./cli/verification.js";
 const packageJson = JSON.parse(readFileSync("./package.json", "utf8"));
 
 const program = new Command();
@@ -42,5 +43,10 @@ program
   .action(forget);
 
 program.command("email").description("Configure email settings").action(email);
+
+program
+  .command("verify")
+  .description("Verify authentication setup")
+  .action(verification);
 
 program.parse(process.argv);
