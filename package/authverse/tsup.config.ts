5198718d7050367f556f85d37fc4f95cbd7080b0
Abdirahman mohamoud <87425511+abdirahmanmahamoud@users.noreply.github.com>
2026-08-10 20:40:13 +0300

    chore(deps-dev): initialize authverse package structure with build configuration and upgrade TypeScript to 7.0.2 (#289)
    * feat: initialize authverse package structure with build configuration and upgrade TypeScript to 7.0.2
    
    * feat: add tsconfig.build.json to configure build output and declaration generation

--- package/authverse/tsup.config.ts
+++ package/authverse/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["index.ts"],
   format: ["cjs", "esm"],
-  dts: true,
+  dts: false,
   outDir: "dist",
   platform: "node",
   tsconfig: "tsconfig.build.json",
