1f47769d9ba33c50f492fbcb2abdf48512d22223
Abdirahman mohamoud <87425511+abdirahmanmahamoud@users.noreply.github.com>
2026-09-13 19:13:12 +0300

    feat: add refactor, docs, and ci sections to changelog config (#318)

--- package/authverse/changelogithub.config.js
+++ package/authverse/changelogithub.config.js
@@ -5,6 +5,9 @@ export default {
     feat: { title: "Features" },
     fix: { title: "Bug Fixes" },
     test: { title: "Tests" },
+    refactor: { title: "Refactor" },
+    docs: { title: "Documentation" },
+    ci: { title: "CI" },
   },
   commitUrlFormat: "{{host}}/{{owner}}/{{repository}}/commit/{{hash}}",
   compareUrlFormat:
